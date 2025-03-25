11355b29ed0cd9e669887fc00caa55b35bbb27ba
Chaemin Lee <dge3179@ajou.ac.kr>
2025-03-25 09:46:23 +0900

    Time: 0 ms (100%), Space: 53.7 MB (57.49%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -5,18 +5,35 @@
 var isValid = function(s) {
     let ans = false;
     const stack = [];
+    const pairs = {
+        '(': ')',
+        '{': '}',
+        '[': ']',
+    }
+
+    if (s.length % 2 !== 0) return false;
+
 
     for (const str of s) {
-        
-        if (str === ')' && stack.at(-1) === '(') {
-            stack.pop();
-        } else if (str === ']' && stack.at(-1) === '[') {
-            stack.pop();
-        } else if (str === '}' && stack.at(-1) === '{') {
-            stack.pop();
-        } else {
+        if (pairs.hasOwnProperty(str)) {
             stack.push(str);
+            continue;
         }
+        
+        if (pairs[stack[stack.length-1]] === str) {
+            stack.pop();
+            continue;
+        }
+        return false;
+        // if (str === ')' && stack.at(-1) === '(') {
+        //     stack.pop();
+        // } else if (str === ']' && stack.at(-1) === '[') {
+        //     stack.pop();
+        // } else if (str === '}' && stack.at(-1) === '{') {
+        //     stack.pop();
+        // } else {
+        //     stack.push(str);
+        // }
 
         console.log(stack)
     }
